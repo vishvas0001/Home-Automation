a977368c7dc4c11e330af67cb204aa151098db86
VISHVAS <112040771+vishvas0001@users.noreply.github.com>
2022-10-18 17:45:14 +0530

    Add files via upload

---
+++ Home_Automation.ino
@@ -0,0 +1,35 @@
+# (To Control LED light from Laptop/Pc)
+
+void setup()
+{
+pinMode(D4,OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(D4,0);
+delay(100);
+digitalWrite(D4,1);
+delay(100);
+}
+
+
+
+# (To Control LED light from Mobile Phone)
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = "TOKEN-ID"
+char ssid[] = "VISHVAS"
+char Pass[] = "12345678"
+
+void setup()
+{
+Blynk.begin(auth,ssid,Pass);
+}
+
+void loop()
+{
+Blynk.run();
+}
